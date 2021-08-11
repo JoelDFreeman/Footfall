6cabf795a7e84eb1e3a62f1be3c3d923db87d13c
JoelDFreeman <75079103+JoelDFreeman@users.noreply.github.com>
2021-08-11 02:42:28 -0700

    Add files via upload

---
+++ filters.py
@@ -0,0 +1,26 @@
+import django_filters
+from django_filters import DateFilter, CharFilter
+
+from .models import *
+
+class SafetyFormFilter(django_filters.FilterSet):
+	start_date = DateFilter(field_name="date_created", lookup_expr='gte')
+	end_date = DateFilter(field_name="date_completion", lookup_expr='lte')
+	note = CharFilter(field_name='form_note', lookup_expr='icontains')
+
+
+	class Meta:
+		model = SafetyForm
+		fields = '__all__'
+		exclude = [
+			"date_created",
+			'description',
+			'form_note',
+			'user',
+			'date_completion',
+			'SafetyCheck',]		
+
+
+
+
+				
